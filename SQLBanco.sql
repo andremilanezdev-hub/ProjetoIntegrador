ec0e2a57b9140bb78234821e5353ba03d82b3916
André Milanez <andremilanezdev@gmail.com>
2026-06-08 21:06:55 -0300

    Nova versão do SQL
    Ajustado compatibilidade de mysql

--- SQLBanco.sql
+++ SQLBanco.sql
@@ -1,20 +1,12 @@
 -- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Schema controle_visitantes
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema controle_visitantes
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `controle_visitantes` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+CREATE SCHEMA IF NOT EXISTS `controle_visitantes` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 USE `controle_visitantes` ;
 
 -- -----------------------------------------------------
@@ -27,8 +19,7 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`departamentos` (
 ENGINE = InnoDB
 AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `controle_visitantes`.`usuarios`
@@ -39,12 +30,11 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`usuarios` (
   `login` VARCHAR(50) NOT NULL,
   `senha` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `login` (`login` ASC) )
+  UNIQUE INDEX `login` (`login` ASC))
 ENGINE = InnoDB
 AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `controle_visitantes`.`agendamentos`
@@ -58,8 +48,8 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`agendamentos` (
   `motivo` VARCHAR(255) NULL DEFAULT NULL,
   `status` ENUM('AGENDADO', 'CONFIRMADO', 'CANCELADO') NULL DEFAULT 'AGENDADO',
   PRIMARY KEY (`id`),
-  INDEX `departamento_id` (`departamento_id` ASC) ,
-  INDEX `usuario_id` (`usuario_id` ASC) ,
+  INDEX `departamento_id` (`departamento_id` ASC),
+  INDEX `usuario_id` (`usuario_id` ASC),
   CONSTRAINT `agendamentos_ibfk_1`
     FOREIGN KEY (`departamento_id`)
     REFERENCES `controle_visitantes`.`departamentos` (`id`),
@@ -69,8 +59,7 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`agendamentos` (
 ENGINE = InnoDB
 AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `controle_visitantes`.`visitantes`
@@ -82,12 +71,11 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`visitantes` (
   `telefone` VARCHAR(20) NULL DEFAULT NULL,
   `empresa` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `cpf` (`cpf` ASC) )
+  UNIQUE INDEX `cpf` (`cpf` ASC))
 ENGINE = InnoDB
 AUTO_INCREMENT = 4
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `controle_visitantes`.`agendamentos_visitantes`
@@ -97,8 +85,8 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`agendamentos_visitantes` (
   `agendamento_id` INT NOT NULL,
   `visitante_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `agendamento_id` (`agendamento_id` ASC) ,
-  INDEX `visitante_id` (`visitante_id` ASC) ,
+  INDEX `agendamento_id` (`agendamento_id` ASC),
+  INDEX `visitante_id` (`visitante_id` ASC),
   CONSTRAINT `agendamentos_visitantes_ibfk_1`
     FOREIGN KEY (`agendamento_id`)
     REFERENCES `controle_visitantes`.`agendamentos` (`id`),
@@ -108,8 +96,7 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`agendamentos_visitantes` (
 ENGINE = InnoDB
 AUTO_INCREMENT = 4
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `controle_visitantes`.`visitas`
@@ -122,8 +109,8 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`visitas` (
   `data_saida` DATETIME NULL DEFAULT NULL,
   `motivo` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_visita_departamento` (`departamento_id` ASC) ,
-  INDEX `fk_visita_usuario` (`usuario_id` ASC) ,
+  INDEX `fk_visita_departamento` (`departamento_id` ASC),
+  INDEX `fk_visita_usuario` (`usuario_id` ASC),
   CONSTRAINT `fk_visita_departamento`
     FOREIGN KEY (`departamento_id`)
     REFERENCES `controle_visitantes`.`departamentos` (`id`),
@@ -133,8 +120,7 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`visitas` (
 ENGINE = InnoDB
 AUTO_INCREMENT = 4
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `controle_visitantes`.`visitantes_visitas`
@@ -144,8 +130,8 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`visitantes_visitas` (
   `visitantes_id` INT NOT NULL,
   `visitas_id` INT NOT NULL,
   PRIMARY KEY (`idvisitantes_visitas`, `visitantes_id`, `visitas_id`),
-  INDEX `fk_visitantes_visitas_visitantes1_idx` (`visitantes_id` ASC) ,
-  INDEX `fk_visitantes_visitas_visitas1_idx` (`visitas_id` ASC) ,
+  INDEX `fk_visitantes_visitas_visitantes1_idx` (`visitantes_id` ASC),
+  INDEX `fk_visitantes_visitas_visitas1_idx` (`visitas_id` ASC),
   CONSTRAINT `fk_visitantes_visitas_visitantes1`
     FOREIGN KEY (`visitantes_id`)
     REFERENCES `controle_visitantes`.`visitantes` (`id`),
@@ -155,10 +141,8 @@ CREATE TABLE IF NOT EXISTS `controle_visitantes`.`visitantes_visitas` (
 ENGINE = InnoDB
 AUTO_INCREMENT = 5
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+COLLATE = utf8mb4_unicode_ci;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
