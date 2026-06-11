90e417aa760450fc8f2ca9943a7f317e8060e9f6
Douglas <destevam02@gmail.com>
2026-06-10 21:15:11 -0300

    Criacao tela Visitas_Visitantes Ajustado SQL que consulta a tabela visitantes_visitas com seus respectivos JOIN.

--- src/visitas_visitantes.java
+++ src/visitas_visitantes.java
@@ -58,7 +58,10 @@ public class visitas_visitantes {
            }while (opcao !=4);
     }
     private static void listar() throws SQLException {
-        ResultSet rs = conexao_banco.getSelect("SELECT * FROM visitantes_visitas");
+        ResultSet rs = conexao_banco.getSelect("SELECT vt.nome, v.data_entrada,v.data_saida,v.motivo\n" +
+                                                   " FROM visitantes_visitas vv\n" +
+                                                   "inner join visitantes vt on vv.visitantes_id = vt.id\n" +
+                                                   "inner join visitas v on vv.visitas_id =  v.id;");
         System.out.println("Id   -  visitantes_visitas ");
         while (rs.next()) {
             System.out.printf(" %d - %s \n", rs.getInt("id"), rs.getString("nome"));
